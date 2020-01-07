9c113cdbb2a6bafd60b0b796d6aab5d056f529ce
devops <devops.roopa@gmail.com>
2020-01-07 15:50:13 +0530

    honda1.0.commit

--- honda.py
+++ honda.py
@@ -1,6 +1,10 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 bbdnbchzgvb zahggZVCVb
 shjgcjfjchv hZVBCHKJ
+=======
+bbdnbchzgvb z
+>>>>>>> e8a169c... honda1.0.commit
 =======
 bbdnbchzgvb z
 >>>>>>> e8a169c... honda1.0.commit
